5e61cc170d4049eaa11ff53fd9c8745871ceb034
Nghi <nghic8ntrai@gmail.com>
2021-07-16 16:06:04 +0700

    set up redux-persist

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,19 +1,19 @@
 import {configureStore} from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
-// import {persistReducer} from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// import AsyncStorage from '@react-native-community/async-storage';
-// import thunk from 'redux-thunk';
-// const persistConfig = {
-//   key: 'root',
-//   storage: AsyncStorage,
-// };
-// const pReducer = persistReducer(persistConfig, rootReducer);
-// const store = configureStore({
-//   reducer: pReducer,
-//   middleware: [thunk],
-// });
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import AsyncStorage from '@react-native-community/async-storage';
+import thunk from 'redux-thunk';
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+const pReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: pReducer,
+  middleware: [thunk],
 });
+// const store = configureStore({
+//   reducer: rootReducer,
+// });
 export default store;
