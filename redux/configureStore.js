5a7c8a6a5e14739d542b45ab302336e6baa24bcf
Nghi <nghic8ntrai@gmail.com>
2021-07-29 14:53:11 +0700

    set up flipper

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,17 +1,21 @@
 import {configureStore} from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
 import {persistReducer} from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 import AsyncStorage from '@react-native-community/async-storage';
 import thunk from 'redux-thunk';
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
 };
+let middlewareToApply = [thunk];
+if (__DEV__) {
+  const createFlipperDebugger = require('redux-flipper').default;
+  middlewareToApply.push(createFlipperDebugger());
+}
 const pReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
   reducer: pReducer,
-  middleware: [thunk],
+  middleware: middlewareToApply,
 });
 // const store = configureStore({
 //   reducer: rootReducer,
